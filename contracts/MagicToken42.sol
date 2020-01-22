a3ac01eba21ddcd89d2dcbaf39848fcfa89716d4
Martin Verzilli <mverzilli@manas.tech>
2020-01-22 20:56:18 +0100

    Check token metadata owner signature

--- contracts/MagicToken42.sol
+++ contracts/MagicToken42.sol
@@ -2,21 +2,36 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract MagicToken42 is ERC721Full {
+contract MagicToken42 is ERC721Full, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
   constructor() ERC721Full("MagicToken42", "MT42") public {
   }
 
-  function awardItem(address to, string memory tokenURI) public returns (uint256) {
+  function awardItem(address to, string memory tokenUri, bytes32 tokenURIHash, bytes memory signedTokenURI) public returns (uint256) {
+    bytes32 checkHash = hashTokenUri(tokenUri);
+    require(checkHash == tokenURIHash, "Invalid Token URI hash");
+
+    bytes32 ethSigned = ECDSA.toEthSignedMessageHash(tokenURIHash);
+    address signer = ECDSA.recover(ethSigned, signedTokenURI);
+
+    require(signer != address(0), "Invalid Token Metadata signer");
+    require(signer == owner(), "Token metadata was not signed by MT42 owner");
+
     _tokenIds.increment();
 
     uint256 newItemId = _tokenIds.current();
     _mint(to, newItemId);
-    _setTokenURI(newItemId, tokenURI);
+    _setTokenURI(newItemId, tokenUri);
 
     return newItemId;
+  }
+
+  function hashTokenUri(string memory tokenUri) public pure returns (bytes32) {
+    return keccak256(abi.encode(tokenUri));
   }
 }
--- test/magictoken42.test.js
+++ test/magictoken42.test.js
@@ -1,9 +1,31 @@
-const { accounts, contract } = require('@openzeppelin/test-environment');
+const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
 
 const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
 const MagicToken42 = contract.fromArtifact('MagicToken42');
+
+function fixSignature (signature) {
+  // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+  // signature malleability if version is 0/1
+  // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+  let v = parseInt(signature.slice(130, 132), 16);
+  if (v < 27) {
+    v += 27;
+  }
+  const vHex = v.toString(16);
+  return signature.slice(0, 130) + vHex;
+}
+
+async function sign(contract, msg, address) {
+  const hash = await contract.hashTokenUri(msg);
+  const signature = fixSignature(await web3.eth.sign(hash, address));
+
+  return {
+    hash,
+    signature
+  }
+}
 
 describe('MagicToken42', function () {
   const [ owner, other ] = accounts;
@@ -15,13 +37,30 @@ describe('MagicToken42', function () {
   })
 
   it('awards item to buyer', async function () {
-    const txReceipt = await theContract.awardItem(other, 'foo.bar', { from: other });
+    const uri = 'foo.bar';
+    const { hash, signature } = await sign(theContract, uri, owner);
 
-    console.log(txReceipt.logs[0]);
+    const txReceipt = await theContract.awardItem(
+      other,
+      uri,
+      hash,
+      signature,
+      { from: other }
+    );
 
     const newTokenId = txReceipt.logs[0].args.tokenId;
 
     expect((await theContract.ownerOf(newTokenId, { from: other }))
       .toString()).to.equal(other);
+  });
+
+  it('fails if uri not signed by owner', async function () {
+    const uri = 'foo.bar';
+    const { hash, signature } = await sign(theContract, uri, other);
+
+    await expectRevert(
+      theContract.awardItem(other, uri, hash, signature, { from: other }),
+      'Token metadata was not signed by MT42 owner'
+    )
   });
 });
